f0ee8eebc3ca6c55b446c07d943666e27a56923a
adevopg <133045315+adevopg@users.noreply.github.com>
2023-05-24 19:59:58 +0200

    Implementado algunos Scripts

--- src/common/Common.h
+++ src/common/Common.h
@@ -64,10 +64,13 @@ constexpr auto IN_MILLISECONDS = 1000;
 enum AccountTypes
 {
     SEC_PLAYER         = 0,
-    SEC_MODERATOR      = 1,
-    SEC_GAMEMASTER     = 2,
-    SEC_ADMINISTRATOR  = 3,
-    SEC_CONSOLE        = 4                                  // must be always last in list, accounts must have less security level always also
+    SEC_VIP            = 1,
+    SEC_SUBSCRIPTOR    = 2,
+    SEC_MODERATOR      = 3,
+    SEC_GAMEMASTER     = 4,
+    SEC_ADMINISTRATOR  = 5,
+    SEC_OWNER          = 6,
+    SEC_CONSOLE        = 7                                  // must be always last in list, accounts must have less security level always also
 };
 
 enum LocaleConstant
